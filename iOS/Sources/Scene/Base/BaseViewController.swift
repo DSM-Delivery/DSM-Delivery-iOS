1998e62ced563b249e0d2575b046c91667acd453
juyeong <juyeongno1@naver.com>
2023-02-05 16:47:16 +0900

    ♻️ :: BaseViewController 수정

--- iOS/Sources/Scene/Base/BaseViewController.swift
+++ iOS/Sources/Scene/Base/BaseViewController.swift
@@ -8,7 +8,7 @@ class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = DSMDeliveryColor.light.color
         addView()
         setLayout()
         configureVC()
