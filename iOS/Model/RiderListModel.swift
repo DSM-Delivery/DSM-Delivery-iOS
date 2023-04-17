7ade45d9da1da0bfcf4361cf9b4d41f98e1ea1c4
doyeon <parkdoyeon3239@gmail.com>
2023-04-17 11:28:49 +0900

    ♻️ :: RiderListModel 수정

--- iOS/Model/RiderListModel.swift
+++ iOS/Model/RiderListModel.swift
@@ -6,20 +6,19 @@ struct RiderListModel: Codable {
         case riderList = "post_list"
     }
 }
-
 struct RiderList: Codable {
     let title: String
     let userName: String
     let cost: String
     let postId: String
-    let profileImage: String
+    let profileImg: String
     let star: Int
     enum CodingKeys: String, CodingKey {
         case title
         case userName = "user_name"
         case cost
         case postId = "post_id"
-        case profileImage = "profile_img"
+        case profileImg = "profile_img"
         case star
     }
 }
